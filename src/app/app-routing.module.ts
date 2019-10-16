d83373c09b4a37d79fcae237353dba3ae5679294
Gabriel Escorche <gabrielescorche@gmail.com>
2019-10-15 20:34:50 -0430

    Fin de la sección 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,20 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
 
-const app_routes: Routes = [
-    {path:'', component: PortafolioComponent},
-    {path:'about', component: AboutComponent},
-    {path:'item', component: ItemComponent},
-    {path:'**', pathMatch: 'full', redirectTo:''}
-]
+const appRoutes: Routes = [
+    {path: 'home', component: PortafolioComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'item', component: ItemComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-    imports:[
-        RouterModule.forRoot(app_routes)
+    imports: [
+        RouterModule.forRoot( appRoutes, { useHash: true } )
     ],
-    exports:[
+    exports: [
         RouterModule
     ]
 })
 
-export class AppRoutingModule{}
+export class AppRoutingModule {}
