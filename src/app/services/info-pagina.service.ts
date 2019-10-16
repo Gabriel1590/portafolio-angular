3d04aebb90e5f29dee569e615a2187f40c0e43cd
Gabriel Escorche <gabrielescorche@gmail.com>
2019-10-16 01:42:53 -0430

    4ta sección completada

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = { };
+  cargada = false;
+
+
+
+  constructor( private http: HttpClient) {
+
+    //  console.log('Servicio infoPagina listo');
+
+    // Leer el JSON
+
+    this.http.get('assets/data/data_pagina.json')
+      .subscribe ( (resp: InfoPagina ) => {
+
+        this.cargada = true;
+        this.info = resp;
+        console.log(resp);
+
+    });
+
+   }
+}
--- src/app/share/headder/headder.component.ts
+++ src/app/share/headder/headder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-headder',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeadderComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _servicio: InfoPaginaService ) { }
 
   ngOnInit() {
   }
